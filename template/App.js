00f87a92aadf6414a4abc5d298dbfe0508545b53
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2019-01-25 22:20:42 -0800

    got app working

---
+++ template/App.js
@@ -0,0 +1,167 @@
+import * as Expo from 'expo';
+import React from 'react';
+import t from 'tcomb-form-native';
+import { Pedometer } from 'expo';
+import { StyleSheet, Text, View, TextInput, Picker, Image } from 'react-native';
+
+export default class PedometerSensor extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPedometerAvailable: "unknown",
+      pastStepCount: 0,
+      currentStepCount: 0,
+      days: 1
+    }
+  }
+
+  componentDidMount() {
+    this._subscribe();
+  }
+
+  componentDidUpdate() {
+      const end = new Date();
+      const start = new Date();
+      start.setDate(end.getDate() - Number(this.state.days));
+      Pedometer.getStepCountAsync(start, end).then(
+        result => {
+          this.setState({ pastStepCount: result.steps });
+        },
+        error => {
+          this.setState({
+            pastStepCount: "Could not get stepCount: " + error
+          });
+        }
+      );
+  }
+
+  componentWillUnmount() {
+    this._unsubscribe();
+    window.removeEventListener('beforeunload', this._unsubscribe);
+  }
+
+    _subscribe = () => {
+      this._subscription = Pedometer.watchStepCount(result => {
+        this.setState({
+          currentStepCount: result.steps
+        });
+      });
+  
+      Pedometer.isAvailableAsync().then(
+        result => {
+          this.setState({
+            isPedometerAvailable: String(result)
+          });
+        },
+        error => {
+          this.setState({
+            isPedometerAvailable: "Could not locate device's pedometer" + error
+          });
+        }
+      );
+  
+      const end = new Date();
+      const start = new Date();
+      start.setDate(end.getDate() - this.state.days);
+      Pedometer.getStepCountAsync(start, end).then(
+        result => {
+          this.setState({ pastStepCount: result.steps });
+        },
+        error => {
+          this.setState({
+            pastStepCount: "Could not get stepCount: " + error
+          });
+        }
+      );
+    };
+
+  _unsubscribe = () => {
+    this._subscription && this._subscription.remove();
+    this._subscription = null;
+  };
+
+
+  render() {
+    return (
+      <>
+      <View style={styles.container}>
+        <Image source={require('./src/images/quick-step.png')} />
+        <Text style={[styles.main]}>
+        {this.state.days} Day Recap 
+        </Text>
+        <Text style={styles.stats}>
+        {this.state.pastStepCount} Total Steps
+        </Text>
+        <Text style={styles.description}>
+          Scroll to choose a time frame:
+        </Text>
+        <Picker
+        selectedValue={this.state.days}
+        style={{height: 50, width: 100}}
+        itemStyle = {styles.pickerItem}
+        onValueChange={(itemValue, itemIndex) =>
+          this.setState({days: itemValue})
+        }>
+        <Picker.Item style={[styles.pickerItem]} label="24 Hours" value="1" />
+        <Picker.Item label="48 Hours" value="2" />
+        <Picker.Item label="72 hours" value="3" />
+        <Picker.Item label="4 Days" value="4" />
+        <Picker.Item label="5 Days" value="5" />
+        <Picker.Item label="6 Days" value="6" />
+        <Picker.Item label="7 Days" value="7" />
+      </Picker>
+      </View>
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flex: 1,
+    backgroundColor: "#00B29E",
+    marginTop: 25,
+    alignItems: "center",
+    paddingTop: 30,
+
+  },
+  container: {
+    marginTop: 25,
+    paddingTop: 35,
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#00B29E",
+    // justifyContent: "flex-start",
+  },
+  main: {
+    fontSize: 62,
+    fontFamily: "HelveticaNeue-Thin",
+    letterSpacing: 2.5,
+    marginTop: 40,
+    marginBottom: 15,
+    color: '#00FFE2',
+  },
+  stats: {
+    fontSize: 30,
+    fontFamily: "HelveticaNeue-CondensedBold",
+    textTransform: "uppercase",
+    letterSpacing: 3.5,
+    color: '#fafafa',
+  },
+  pickerItem: {
+    color: '#00FFE2',
+    fontFamily: "HelveticaNeue-Light",
+    letterSpacing: 2,
+    fontSize: 22,
+  },
+  description: {
+    fontFamily: "HelveticaNeue-Light",
+    fontSize: 18,
+    marginTop: 12,
+    color: '#fafafa',
+    letterSpacing:.1,
+  }
+});
+
+Expo.registerRootComponent(PedometerSensor);
